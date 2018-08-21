830d38a55cbd6745472a45b7ea792d37cdcbdc28
Karen Giselle Velázquez Hernández <42345693+Karen-Velazquez-Hernandez-5IM9@users.noreply.github.com>
2018-08-20 20:55:37 -0500

    Hola
    Clase Hola con hola mundo

---
+++ Hola/src/hola/Hola.java
@@ -0,0 +1,12 @@
+package hola;
+/**
+ *
+ * @author Karen Velázquez
+ */
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola, ¿cómo estamos?");
+        System.out.println("Karen Velázquez, 5IM9");
+    }
+    
+}
